f9207525f3e44191b5644ea11876defed212fc1c
AlaaMetwally <alaametwally4@gmail.com>
2022-06-12 13:52:48 +0200

    adding account service and logging service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -9,9 +11,12 @@ export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
-  
+
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+  }
+
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.statusUpdated.emit(status);
+    this.accountService.updateStatus(this.id, status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
@@ -9,11 +11,11 @@ export class NewAccountComponent {
 
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
+
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
